a50d937f0516e2f01a9cb458cb2195b5d8d3ace8
Enes Cakir <enes@cakir.web.tr>
2025-03-08 11:13:01 +0300

    catch signal

--- src/post.js
+++ src/post.js
@@ -7,25 +7,35 @@ async function post() {
     core.info(`Waiting for ${waitMinutes} minutes to allow SSH access...`);
     
     // Create a promise that can be canceled
-    let waitTimeout;
-    const waitPromise = new Promise(resolve => {
-      waitTimeout = setTimeout(resolve, waitMinutes * 60 * 1000);
-    });
+    // let waitTimeout;
+    // const waitPromise = new Promise(resolve => {
+    //   waitTimeout = setTimeout(resolve, waitMinutes * 60 * 1000);
+    // });
 
-    // Set up signal handlers for job cancellation
-    const signalHandler = async () => {
-      console.log("test")
-      core.info('Job cancellation detected. Cancelling wait early...');
-      clearTimeout(waitTimeout); // Clear the timeout
-      process.exit(0); // Exit gracefully
-    };
+    // // Set up signal handlers for job cancellation
+    // const signalHandler = async () => {
+    //   console.log("test")
+    //   core.info('Job cancellation detected. Cancelling wait early...');
+    //   clearTimeout(waitTimeout); // Clear the timeout
+    //   process.exit(0); // Exit gracefully
+    // };
     
     // Listen for termination signals
-    process.on('SIGINT', signalHandler);
-    process.on('SIGTERM', signalHandler);
-    
+    // process.on('SIGINT', signalHandler);
+    // process.on('SIGTERM', signalHandler);
+    process.on('SIGINT', () => {
+      console.log('Received SIGINT');
+    });
+    process.on('SIGTERM', () => {
+      console.log('Received SIGTERM');
+    });
+    process.on('exit', () => {
+      console.log('Received exit');
+    });
+
+    await new Promise(resolve => setTimeout(resolve, waitMinutes * 60 * 1000));
     // Wait for the timeout or until a signal is received
-    await waitPromise;
+    // await waitPromise;
 
   } catch (error) {
     core.setFailed(`Post action failed with error: ${error.message}`);
